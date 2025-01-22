ce38917b686f47850e1c809aa58cddabd55e0017
Nest <58371847+Nest4u@users.noreply.github.com>
2025-01-22 16:25:22 +0100

    Update main.go

--- main.go
+++ main.go
@@ -26,13 +26,7 @@ func main() {
 	r := gin.Default()
 
 	
-	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"https://front-trood.vercel.app"}, 
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-	}))
+
 
 	
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
